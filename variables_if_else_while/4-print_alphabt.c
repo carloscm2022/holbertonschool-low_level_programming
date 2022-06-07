dea49e4d60199145b7c4ffe768f774d4db103f4f
Carlos <carloscr7cmreal@gmail.com>
2022-06-07 15:37:30 -0700

    subiendo cambios

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+		{
+			putchar(alphabet);
+		}
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
