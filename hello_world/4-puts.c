636580133c32129179b056278ca029f179f97038
Carlos <carloscr7cmreal@gmail.com>
2022-06-07 11:20:36 -0700

    subiendo cambios

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print the sentence "Programming is like building a multilingual puzzle
+ * main - Entry point
  *
  * return: Always 0 (Success)
  */
@@ -8,4 +8,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
