27fc492a959d68812e57f97ef66f400c7b4c0c73
Carlos <carloscr7cmreal@gmail.com>
2022-06-09 14:09:30 -0700

    subiendo cambios

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,9 +2,8 @@
 /**
  * main - prints the alphabet, in lowercase
  * followed by a new line
- * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char alphabet;
 
