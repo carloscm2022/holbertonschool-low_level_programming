333c9e3e3bb95562d72657dd11a74c144a7fc58b
Carlos <carloscr7cmreal@gmail.com>
2022-06-12 19:10:50 -0700

    subiendo codificación

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,10 +1,9 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - computes and prints the sum of all the multiples
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i, sum = 0;
