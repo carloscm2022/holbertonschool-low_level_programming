a7e4d0d2083d11ddc5d4c5c7f9944669d88f6354
Carlos <carloscr7cmreal@gmail.com>
2022-06-14 14:15:22 -0700

    New Proyect Holbi

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,1 @@
-#include "main.h"
 
-/**
- * _isupper - checks for uppercase character
- * @c: ASCII number
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isupper(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
