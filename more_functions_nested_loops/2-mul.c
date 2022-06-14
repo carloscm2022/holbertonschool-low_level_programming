9a4cd6ebf7a8ba330a822204d54400174b482716
Carlos <carloscr7cmreal@gmail.com>
2022-06-14 14:53:57 -0700

    subiendo proyect codificación

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: number a
+ * @b: number b
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
