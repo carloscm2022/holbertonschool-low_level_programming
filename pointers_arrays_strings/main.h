3575881d4031fd0b18788fad1fab115900edb4c3
Carlos <carloscr7cmreal@gmail.com>
2022-06-19 21:57:07 -0700

    new project

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -55,4 +55,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 /*task 5*/
 char *_strstr(char *haystack, char *needle);
+/*task 6*/
+void print_chessboard(char (*a)[8]);
 #endif
