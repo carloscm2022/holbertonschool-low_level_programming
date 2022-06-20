e009a799dc6510aeba6797f9b66658ff66adcc7b
Carlos <carloscr7cmreal@gmail.com>
2022-06-19 21:06:12 -0700

    new project 2022

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,5 +32,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 /*task 2*/
 char *_strncpy(char *dest, char *src, int n);
+/*task 3*/
+int _strcmp(char *s1, char *s2);
 
 #endif
