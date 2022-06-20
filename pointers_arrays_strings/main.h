a0122e5fa63c15ff0296cb75ef4c404ebd48dffc
Carlos <carloscr7cmreal@gmail.com>
2022-06-19 23:11:55 -0700

    uploading new project

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -46,8 +46,9 @@ char *leet(char *);
 char *rot13(char *);
 /*task 9*/
 void print_number(int n);
-/*task 10*/
-void print_number(int n);
+/*task 11*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /*Project 4*/
 /*task 0*/
 char *_memset(char *s, char b, unsigned int n);
