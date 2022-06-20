339ec658a2199816ae41e14f2bbce45047468be8
Carlos <carloscr7cmreal@gmail.com>
2022-06-19 20:55:21 -0700

    uploading project new

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,15 +1,32 @@
 #ifndef _CUSTOMIZE_LIBRARIES_
 #define _CUSTOMIZE_LIBRARIES_
+/*Project 2*/
+/*task 0*/
 int _putchar(char c);
+/*task 1*/
 void reset_to_98(int *n);
+/*task 2*/
 void swap_int(int *a, int *b);
+/*task 3*/
 int _strlen(char *s);
+/*task 4*/
 void _puts(char *str);
+/*task 5*/
 void print_rev(char *s);
+/*task 6*/
 void rev_string(char *s);
+/*task 7*/
 void puts2(char *str);
+/*task 8*/
 void puts_half(char *str);
+/*task 9*/
 void print_array(int *a, int n);
+/*task 10*/
 char *_strcpy(char *dest, char *src);
+/*task 11*/
 int _atoi(char *s);
+/*Project 3*/
+/*task 0*/
+char *_strcat(char *dest, char *src);
+
 #endif
