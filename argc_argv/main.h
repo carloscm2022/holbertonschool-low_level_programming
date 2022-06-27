9def101ea2b3ff0730b0acf0a8206cfea53bb750
Carlos <carloscr7cmreal@gmail.com>
2022-06-26 20:38:19 -0700

    uploading project

---
+++ argc_argv/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(int argc __attribute__((unused)), char *argv[]);
+
+int main(int argc, char *argv[] __attribute__((unused)));
+
+int main(int argc, char *argv[]);
+
+int _putchar(char c);
+
+#endif
