4edef73b12a1a816523ece9ca428ded2ced3abf2
Carlos <carloscr7cmreal@gmail.com>
2022-07-03 21:18:48 -0700

    project advanced finish

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -1,38 +1,47 @@
-#include "f_pointrs.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - prints the opcodes of its own main function
- * @argc: line numbers of arguments
- * @argv: array strings
- * Return: 0
+ * main - Prints its own opcodes.
  *
- * ./main number_of_bytes: 55
- * objdump -d -j.text -M intel main
+ * @argc: Quantity of arguments.
+ *
+ * @argv: Vector of arguments.
+ *
+ * Return: 0 if success
+ * 1 if exist more than 2 argument on the argv
+ * 2 if the numbers of bytes passed by the arguments is negative
  */
 
 int main(int argc, char *argv[])
 {
-	register int i, n;
-	char *ptr = (char *)main;
+	int number_bytes = 0, i = 0;
+	char *f = NULL;
 
 	if (argc != 2)
 	{
-		printf("Error\n"), exit(1);
+		printf("Error\n");
+		exit(1);
 	}
-	n = atoi(argv[1]);
 
-	if (n < 0)
+	number_bytes = atoi(argv[1]);
+
+	if (number_bytes < 0)
 	{
-		printf("Error\n"), exit(2);
+		printf("Error\n");
+		exit(2);
 	}
 
-	for (i = 0; i < n - 1; i++)
+	f = (char *)main;
+
+	for (i = 0; i < number_bytes; i++)
 	{
-		printf("%02hhx ", ptr[i]);
+		printf("%02hhx", f[i]);
+		if (i + 1 != number_bytes)
+			printf(" ");
+		else
+			printf("\n");
 	}
 
-	printf("%02hhx\n", ptr[i]);
 	return (0);
 }
